cccd457131fcbe04d5846f6c1acfbc8be5827d79
Leohuguenin <leonardohuguenin@live.com>
2025-03-06 23:19:52 +1100

    added a download button

--- script.js
+++ script.js
@@ -14,3 +14,24 @@
   document.getElementById("cassava-crisps").addEventListener("click", function () {
     toggleText("cassava-crisps", "cassava-crisps-info");
   });
+ const downloadBtn = document.getElementById('downloadBtn');
+  const formContainer = document.querySelector('.form-container');
+  const emailInput = document.getElementById('emailInput');
+  const submitBtn = document.getElementById('submitBtn');
+  const submittedMessage = document.querySelector('.submitted-message');
+
+  downloadBtn.addEventListener('click', () => {
+    formContainer.style.display = 'flex';
+    submittedMessage.style.display = 'none';
+  });
+
+  submitBtn.addEventListener('click', () => {
+    formContainer.style.display = 'none';
+    submittedMessage.style.display = 'block';
+    emailInput.value = ''; 
+    
+
+    setTimeout(() => {
+      submittedMessage.style.display = 'none';
+    }, 3000); 
+  });
